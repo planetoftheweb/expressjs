0c602115853afaf7725eade10f45f949a3d5d28b
Ray Villalobos <ray@lynda.com>
2016-06-28 14:26:54 -0400

    01_05e

--- app/app.js
+++ app/app.js
@@ -1,10 +1,21 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var myServer = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type" : "text/html"});
-  response.write('<h1>Roux Meetups</h1>');
-  response.end();
+app.get('/', function(req, res) {
+  res.send('<h1>Roux Academy Meetups</h1>');
 });
 
-myServer.listen(3000);
-console.log('Go to http://localhost:3000 on your browser');
+var server = app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
+
+// var http = require('http');
+//
+// var myServer = http.createServer(function(request, response) {
+//   response.writeHead(200, {"Content-Type" : "text/html"});
+//   response.write('<h1>Roux Meetups</h1>');
+//   response.end();
+// });
+//
+// myServer.listen(3000);
+// console.log('Go to http://localhost:3000 on your browser');
