e33662797ef4b4261c277924833746b746344690
Ray Villalobos <ray@lynda.com>
2016-06-29 16:28:04 -0400

    02_01e

--- app/app.js
+++ app/app.js
@@ -5,6 +5,13 @@ var dataFile = require('./data/data.json');
 app.set('port', process.env.PORT || 3000 );
 
 app.get('/', function(req, res) {
+  res.send(`
+      <h1>Welcome</h1>
+      <p>Roux Academy Meetups put together artists from all walks of life</p>
+  `);
+});
+
+app.get('/speakers', function(req, res) {
   var info = '';
   dataFile.speakers.forEach(function(item) {
     info += `
@@ -20,17 +27,16 @@ app.get('/', function(req, res) {
   `);
 });
 
+app.get('/speakers/:speakerid', function(req, res) {
+
+  var speaker = dataFile.speakers[req.params.speakerid];
+  res.send(`
+      <h1>${speaker.title}</h1>
+      <h2>with ${speaker.name}</h2>
+      <p>${speaker.summary}</p>
+  `);
+});
+
 var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
 });
-
-// var http = require('http');
-//
-// var myServer = http.createServer(function(request, response) {
-//   response.writeHead(200, {"Content-Type" : "text/html"});
-//   response.write('<h1>Roux Meetups</h1>');
-//   response.end();
-// });
-//
-// myServer.listen(3000);
-// console.log('Go to http://localhost:3000 on your browser');
