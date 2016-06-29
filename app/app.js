34c96e4806ee04c292cdd38ace4922a0bd038f7b
Ray Villalobos <ray@lynda.com>
2016-06-28 22:59:58 -0400

    01_06e

--- app/app.js
+++ app/app.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var app = express();
+var dataFile = require('./data/data.json');
+
+app.set('port', process.env.PORT || 3000 );
 
 app.get('/', function(req, res) {
-  res.send('<h1>Roux Academy Meetups</h1>');
+  var info = '';
+  dataFile.speakers.forEach(function(item) {
+    info += `
+    <li>
+      <h2>${item.name}</h2>
+      <p>${item.summary}</p>
+    </li>
+    `;
+  });
+  res.send(`
+      <h1>Roux Academy Meetups</h1>
+      ${info}
+  `);
 });
 
-var server = app.listen(3000, function() {
-  console.log('Listening on port 3000');
+var server = app.listen(app.get('port'), function() {
+  console.log('Listening on port ' + app.get('port'));
 });
 
 // var http = require('http');
