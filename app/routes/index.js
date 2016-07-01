affbc9fb93a3536c0dbbe5aca3409caaefa54f90
Ray Villalobos <ray@lynda.com>
2016-07-01 17:24:53 -0400

    03_03e

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
+  var data = req.app.get('appData');
+  var pagePhotos = [];
+
+  data.speakers.forEach(function(item) {
+    pagePhotos = pagePhotos.concat(item.artwork);
+  });
 
   res.render('index', {
     pageTitle: 'Home',
+    artwork: pagePhotos,
     pageID: 'home'
   });
 
