f37ec7aa29afc2c58f2f24cdf428a0d266c238aa
Ray Villalobos <ray@lynda.com>
2016-07-02 16:41:24 -0400

    03_05e

--- app/routes/speakers.js
+++ app/routes/speakers.js
@@ -2,23 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/speakers', function(req, res) {
-  var info = '';
-  var dataFile = req.app.get('appData');
-  dataFile.speakers.forEach(function(item) {
-    info += `
-    <li>
-      <h2>${item.name}</h2>
-      <img src="/images/speakers/${item.shortname}_tn.jpg" alt="speaker">
-      <p>${item.summary}</p>
-    </li>
-    `;
+  var data = req.app.get('appData');
+  var pagePhotos = [];
+  var pageSpeakers = data.speakers;
+
+  data.speakers.forEach(function(item) {
+    pagePhotos = pagePhotos.concat(item.artwork);
   });
-  res.send(`
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
-      <h1>Roux Academy Meetups</h1>
-      ${info}
-      <script src="/reload/reload.js"></script>
-  `);
+
+  res.render('speakers', {
+    pageTitle: 'Speakers',
+    artwork: pagePhotos,
+    speakers: pageSpeakers,
+    pageID: 'speakers'
+  });
+
 });
 
 router.get('/speakers/:speakerid', function(req, res) {
