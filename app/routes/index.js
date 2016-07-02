6262a4715043a3d43d8f7fbb538dd3e5d6e6b848
Ray Villalobos <ray@lynda.com>
2016-07-02 17:16:53 -0400

    03_05e

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 router.get('/', function(req, res) {
   var data = req.app.get('appData');
   var pagePhotos = [];
+  var pageSpeakers = data.speakers;
 
   data.speakers.forEach(function(item) {
     pagePhotos = pagePhotos.concat(item.artwork);
@@ -12,6 +13,7 @@ router.get('/', function(req, res) {
   res.render('index', {
     pageTitle: 'Home',
     artwork: pagePhotos,
+    speakers: pageSpeakers,
     pageID: 'home'
   });
 
