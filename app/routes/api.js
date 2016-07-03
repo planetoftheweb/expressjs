ec2e67050d2a43b59e3219b73df40fb730f4713f
Ray Villalobos <ray@lynda.com>
2016-07-03 19:54:44 -0400

    04_04e

--- app/routes/api.js
+++ app/routes/api.js
@@ -9,14 +9,29 @@ router.get('/api', function(req, res) {
 });
 
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: false })); 
+router.use(bodyParser.urlencoded({ extended: false }));
 
 router.post('/api', function(req, res) {
   feedbackData.unshift(req.body);
   fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf8', function(err) {
-    console.log(err);
+    if (err) {
+      console.log(err);
+    }
   });
   res.json(feedbackData);
 });
+
+
+router.delete('/api/:id', function(req, res) {
+  feedbackData.splice(req.params.id, 1);
+  fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf8', function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  res.json(feedbackData);
+});
+
+
 
 module.exports = router;
