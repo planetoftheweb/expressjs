da18a809cd775385c96f3a97faa64b12bd45c420
Ray Villalobos <ray@lynda.com>
2016-07-02 20:12:59 -0400

    03_06e

--- app/app.js
+++ app/app.js
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
 app.locals.siteTitle = 'Roux Meetups';
+app.locals.allSpeakers = dataFile.speakers;
 
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
--- app/routes/speakers.js
+++ app/routes/speakers.js
@@ -14,22 +14,28 @@ router.get('/speakers', function(req, res) {
     pageTitle: 'Speakers',
     artwork: pagePhotos,
     speakers: pageSpeakers,
-    pageID: 'speakers'
+    pageID: 'speakerList'
   });
-
 });
 
 router.get('/speakers/:speakerid', function(req, res) {
-  var dataFile = req.app.get('appData');
-  var speaker = dataFile.speakers[req.params.speakerid];
-  res.send(`
-      <link rel="stylesheet" type="text/css" href="/css/style.css">
-      <h1>${speaker.title}</h1>
-      <h2>with ${speaker.name}</h2>
-      <img src="/images/speakers/${speaker.shortname}_tn.jpg" alt="speaker">
-      <p>${speaker.summary}</p>
-      <script src="/reload/reload.js"></script>
-  `);
+  var data = req.app.get('appData');
+  var pagePhotos = [];
+  var pageSpeakers = [];
+
+  data.speakers.forEach(function(item) {
+    if (item.shortname == req.params.speakerid) {
+      pageSpeakers.push(item);
+      pagePhotos = pagePhotos.concat(item.artwork);
+    }
+  });
+
+  res.render('speakers', {
+    pageTitle: 'Speaker Info',
+    artwork: pagePhotos,
+    speakers: pageSpeakers,
+    pageID: 'speakerDetail'
+  });
 });
 
 module.exports = router;
