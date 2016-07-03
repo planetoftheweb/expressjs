c98214f442ec6cdf077cae7103c83153d978a3e6
Ray Villalobos <ray@lynda.com>
2016-07-03 18:20:19 -0400

    04_03e

--- app/public/js/feedback.js
+++ app/public/js/feedback.js
@@ -1,6 +1,15 @@
 $(function() {
   $.getJSON('api', updateFeedback);
 
+  $('.feedback-form').submit(function(e) {
+    e.preventDefault();
+    $.post('api', {
+      name: $('#feedback-form-name').val(),
+      title: $('#feedback-form-title').val(),
+      message: $('#feedback-form-message').val()
+    }, updateFeedback);
+  });
+
   function updateFeedback(data) {
    var output = '';
    $.each(data,function(key, item) {
@@ -8,7 +17,7 @@ $(function() {
      output += '       <div class="feedback-item media">';
      output += '         <div class="feedback-info media-body">';
      output += '           <div class="feedback-head">';
-     output += '             <div class="feedback-title">' + item.title + '<small class="feedback-name label label-info"></small></div>';
+     output += '             <div class="feedback-title">' + item.title + ' <small class="feedback-name label label-info">' + item.name + '</small></div>';
      output += '           </div>';
      output += '           <div class="feedback-message">' + item.message + '</div>';
      output += '         </div>';
--- app/routes/api.js
+++ app/routes/api.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var fs = require('fs');
 var feedbackData = require('../data/feedback.json');
 
 router.get('/api', function(req, res) {
+  res.json(feedbackData);
+});
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false })); 
+
+router.post('/api', function(req, res) {
+  feedbackData.unshift(req.body);
+  fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf8', function(err) {
+    console.log(err);
+  });
   res.json(feedbackData);
 });
 
