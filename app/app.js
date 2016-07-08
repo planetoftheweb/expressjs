dac93b0d0121cb0f6081bb592418c5d09600c565
Ray Villalobos <ray@lynda.com>
2016-07-07 20:00:03 -0400

    05_03e

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var reload = require('reload');
 var app = express();
 var dataFile = require('./data/data.json');
+var io = require('socket.io')();
 
 app.set('port', process.env.PORT || 3000 );
 app.set('appData', dataFile);
@@ -20,6 +21,13 @@ app.use(require('./routes/chat'));
 
 var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
+});
+
+io.attach(server);
+io.on('connection', function(socket) {
+  socket.on('postMessage', function(data) {
+    io.emit('updateMessages', data);
+  });
 });
 
 reload(server, app);
--- app/public/js/chat.js
+++ app/public/js/chat.js
@@ -1,24 +1,37 @@
-var chatForm = document.forms.chatForm;
+var socket = io();
+var chatUsername = document.querySelector('#chat-username');
+var chatMessage = document.querySelector('#chat-message');
 
-if (chatForm) {
-  var chatUsername = document.querySelector('#chat-username');
-  var chatMessage = document.querySelector('#chat-message');
+socket.on('connect', function() {
+  var chatForm = document.forms.chatForm;
 
-  chatForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    showMessage({
-      username: chatUsername.value,
-      message: chatMessage.value,
-    });
-    chatMessage.value='';
-    chatMessage.focus();
-  });
-}
+  if (chatForm) {
+    chatForm.addEventListener('submit', function(e) {
+      e.preventDefault();
+      socket.emit('postMessage',{
+        username: chatUsername.value,
+        message: chatMessage.value,
+      });
+      chatMessage.value='';
+      chatMessage.focus();
+    }); //chatform event
+
+    socket.on('updateMessages', function(data) {
+      showMessage(data);
+    }); //updateMessages
+  } //chatform
+}); //socket
 
 function showMessage(data) {
   var chatDisplay = document.querySelector('.chat-display');
   var newMessage = document.createElement('p');
-  newMessage.className = 'bg-success chat-text';
+
+  if (chatUsername.value == data.username) {
+    newMessage.className = 'bg-success chat-text';
+  } else {
+    newMessage.className = 'bg-info text-warning chat-text';
+  }
+
   newMessage.innerHTML = '<strong>' + data.username + '</strong>: ' + data.message;
   chatDisplay.insertBefore(newMessage, chatDisplay.firstChild);
 }
