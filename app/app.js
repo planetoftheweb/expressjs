8373b8f68b36b0fb4e798091dfe7991b3d2a6b96
hosamsam <hosamelnabawy1996@gmail.com>
2016-12-04 23:09:50 -0800

    udate speakers page

--- app/app.js
+++ app/app.js
@@ -1,15 +1,173 @@
 // |======| To learn new technologies => spend your time on reading its documentation |======|
 // testing CLI pull request
+
+
+
 var express = require('express');
 var app = express();
+var mydate = require('./data/data.json');
 
-app.get('/', function(req, res) {
-  res.send('<h1>Hosam Elnabawy</h1>');
+
+// set our environent variable
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(request, response) {
+
+	response.send(`
+
+		<h2>NodeJs HomePage</h2>
+		<p>nodejs is poweful tool for javascript to run on serverside</p>
+    <a href="/speakers">show speakers</a>
+		`);
+
 });
 
-var server = app.listen(3000, function() {
-  console.log('Listening on port 3000');
+app.get('/speakers', function(req, res) {
+  var speaker = "";
+  mydate.speakers.forEach(function(info) {
+      speaker += `
+        <tr>
+          <td>${info.title}</td>
+          <td><a href="/speakers/${mydate.speakers.indexOf(info)+1}">${info.name}</a></td>
+          <td>${info.summary}</td>
+        </tr>
+      `
+  });
+
+  res.send(`
+  <head>
+  <title>Speakers page</title>
+    <style>
+    *{
+      font-family: sans-serif, tahoma;
+      box-sizing: border-box;
+    }
+      table td {
+        padding: 10px;
+
+      }
+      table thead td {
+        font-weight:bold;
+        border-bottom: 1px solid #999
+      }
+
+      table tbody td:hover {
+            padding: 10px;
+      }
+
+    </style>
+  </head>
+  
+  <body>
+  <h1>Hosam Elnabawy</h1>
+    <table>
+      <thead>
+        <td>TITLE</td>
+        <td>NAME</td>
+        <td>SUMMARY</td>
+      </thead>
+    
+    ${speaker}
+    </table>
+    </body>
+  `);
 });
+
+app.get('/speakers/:id',function(request, response) {
+  var speaker = mydate.speakers[(request.params.id)-1];
+
+  response.send(`<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>${speaker.name}</title>
+        <style>
+        *{box-sizing: border-box;font-family:sans-serif}
+        body {padding: 0;margin: 0}
+            section#title{
+                width: 100%;
+                height: auto;
+                background-color: brown;
+                text-align: center;
+                color: #fff;
+                padding: 20px
+            }
+            table {
+              border-radius: 5px;
+              bodrer: 2px solid #999
+            }
+            ul {
+                padding: 0;
+                margin:0;
+                list-style: none
+            }
+            .container {
+                width: 967px;
+                margin: 0 auto
+            }
+            #title h1 span {
+                font-size: 12px;
+                color: #eee;
+                margin-left: 12px
+            }
+            #work .container >ul >li {
+                width: 50%;
+                padding: 15px;
+                background-color: #eee;
+                border: 1px solid #ccc;
+                border-radius: 5px;
+                float: left
+            }
+        </style>
+    </head>
+    <body>
+        <section id="title">
+            <h1>${speaker.name}<span>- ${speaker.shortname}/span></h1>
+            <h4>${speaker.title}</h4>
+        </section><br>
+        <div class="container">
+        <a href="/speakers">back to speakers page</a> 
+        ${speaker.summary}<hr><br><div>
+        <section id="work">
+            <div class="container">
+
+            
+            <ul>
+                <li>
+                    ${speaker.description}
+                </li>
+                <li class="work">
+                    <ul>
+                        <li>${speaker.artworks}</li>
+                    </ul>
+                </li>
+            </ul>
+            </div>
+        </section>
+    </body>
+
+</html>
+
+`);
+});
+
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Listening on port '+ app.get('port'));
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 // var http = require('http');
 //
